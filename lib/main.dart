47926334dddeb5ad6ca314b1162224ee94672a15
Luiz Filho <luiz@jera.com.br>
2019-12-15 15:39:16 -0400

    Init clean arch and bloc usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-
-import 'home.dart';
+import 'package:mymovies/view/home/bloc/home_bloc.dart';
+import 'package:mymovies/view/home/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(hintColor: Colors.white),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      blocs: [Bloc((i) => HomeBloc())],
+      child: MaterialApp(
+        home: HomePage(),
+        theme: ThemeData(hintColor: Colors.white),
+      ),
+    );
+  }
 }
