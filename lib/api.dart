13a1094e1673f6c8872969399c15844d2cea2259
Luiz Filho <luiz@jera.com.br>
2019-12-14 20:05:09 -0400

    Fix spaces

--- lib/api.dart
+++ lib/api.dart
@@ -8,12 +8,12 @@ class Api {
 
   Future<Map> getGifs() async {
     http.Response response = await http.get(trendingGifsUrl);
-
     return json.decode(response.body);
   }
 
   Future<Map> searchGifs(String text) async {
-    http.Response response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=RBuHVONIgAuw98Ew1x7NDfRGTtNIe6LS&q=$text&limit=26&offset=0&rating=G&lang=en");
+    http.Response response = await http.get(
+        "https://api.giphy.com/v1/gifs/search?api_key=RBuHVONIgAuw98Ew1x7NDfRGTtNIe6LS&q=$text&limit=26&offset=0&rating=G&lang=en");
     return json.decode(response.body);
   }
 }
