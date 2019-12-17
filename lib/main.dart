288bf3fbac519edac51da2a4b6c61097581d94f1
Luiz Filho <luiz@jera.com.br>
2019-12-16 21:49:20 -0400

    Bloc finished, Clean and update to use Dio and Dependency provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-import 'package:mymovies/view/home/bloc/home_bloc.dart';
-import 'package:mymovies/view/home/home.dart';
+import 'package:mymovies/data/remote/api_client.dart';
+import 'package:mymovies/data/repository/default_gif_repository.dart';
+import 'package:mymovies/presentation/home/bloc/home_bloc.dart';
+import 'package:mymovies/presentation/home/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      blocs: [Bloc((i) => HomeBloc())],
+      blocs: [
+        Bloc(
+            (i) => HomeBloc(BlocProvider.getDependency<DefaultGifRepository>()))
+      ],
+      dependencies: [
+        Dependency((i) => ApiClient()),
+        Dependency((i) =>
+            DefaultGifRepository(BlocProvider.getDependency<ApiClient>()))
+      ],
       child: MaterialApp(
         home: HomePage(),
-        theme: ThemeData(hintColor: Colors.white),
+        theme: ThemeData(
+          hintColor: Colors.white,
+        ),
       ),
     );
   }
